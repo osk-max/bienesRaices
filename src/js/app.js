62ece052303c7bd5f10ee0a5eadd02fec63cb75a
Oscar Cantor <ocantorb1889@gmail.com>
2022-05-05 23:19:40 +0200

    Se trebaja la parte de estilos del darkmode

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
 
   eventListeners();
+
+  darkMode();
 });
+
+// Funciones - Eventos
+function darkMode() {
+  const btnDarkMode = document.querySelector('.dark__mode--boton');
+  btnDarkMode.addEventListener('click', function() {
+    document.body.classList.toggle('dark__mode');
+  });  
+}
 
 function eventListeners() {
   const mobileMenu = document.querySelector('.mobile__menu');
@@ -14,5 +24,3 @@ function navegacionResponsive() {
   
   menuNavegacion.classList.toggle('mostrar');  
 }
-
-
