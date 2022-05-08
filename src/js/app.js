c11cac4e7c882b29852e2c8019eb34bf428a0126
Oscar Cantor <ocantorb1889@gmail.com>
2022-05-08 03:48:01 +0200

    Se finaliza configuracion darkmode o blanco segun pref SO

--- src/js/app.js
+++ src/js/app.js
@@ -7,6 +7,22 @@ document.addEventListener('DOMContentLoaded', function() {
 
 // Funciones - Eventos
 function darkMode() {
+  const prefiereDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
+
+  if(prefiereDarkMode.matches) {
+    document.body.classList.add('dark__mode');
+  } else {
+    document.body.classList.remove('dark__mode');
+  }
+
+  prefiereDarkMode.addEventListener('change', function() {
+    if(prefiereDarkMode.matches) {
+      document.body.classList.add('dark__mode');
+    } else {
+      document.body.classList.remove('dark__mode');
+    }
+  });
+
   const btnDarkMode = document.querySelector('.dark__mode--boton');
   btnDarkMode.addEventListener('click', function() {
     document.body.classList.toggle('dark__mode');
