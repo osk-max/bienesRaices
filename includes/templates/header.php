8075600f4da27685d1e1e5033b232b34a3f2c906
Oscar Cantor <ocantorb1889@gmail.com>
2022-05-28 02:23:25 +0200

    Se crea archivo header php en directorio includes y templates

---
+++ includes/templates/header.php
@@ -0,0 +1,23 @@
+<header class="header">
+    <div class="contenedor contenido__header">
+      <div class="barra">
+        <a href="/">
+          <img src="build/img/logo.svg" alt="Logotipo de bienes raices">
+        </a>        
+
+        <div class="mobile__menu">
+          <img src="build/img/barras.svg" alt="icono menu responsive">
+        </div><!-- .mobil__menu -->
+
+        <div class="derecha">
+          <img src="build/img/dark-mode.svg" alt="boton modo oscuro" class="dark__mode--boton">
+          <nav class="navegacion">
+            <a href="nosotros.html">Nosotros</a>
+            <a href="anuncios.html">Anuncios</a>
+            <a href="blog.html">Blog</a>
+            <a href="contacto.html">Contacto</a>
+          </nav><!-- .navegacion -->
+        </div><!-- derecha -->
+
+    </div><!-- .contenedor .contenido__header-->
+  </header><!-- .header -->
