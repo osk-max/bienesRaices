101c9116849a30799f9ecbb2529623298270d440
Oscar Cantor <ocantorb1889@gmail.com>
2022-06-02 18:19:48 +0200

    Trabajando en el footer

---
+++ includes/templates/footer.php
@@ -0,0 +1,11 @@
+<footer class="footer seccion">
+    <div class="contenedor contenedor__footer">
+      <nav class="navegacion">
+        <a href="nosotros.php">Nosotros</a>
+        <a href="anuncios.php">Anuncios</a>
+        <a href="blog.php">Blog</a>
+        <a href="contacto.php">Contacto</a>
+      </nav><!-- .navegacion -->
+    </div><!-- .contenedor .contenedor__footer-->
+    <p class="copyright">Todos los derechos Reservados <?php echo date('Y'); ?> &copy;</p>
+  </footer><!-- .footer .seccion-->
--- nosotros.php
+++ nosotros.php
@@ -45,18 +45,7 @@
     </div><!-- .iconos__nosotros -->
   </main><!-- .contenedor .seccion-->
 
-  <footer class="footer seccion">
-    <div class="contenedor contenedor__footer">
-      <nav class="navegacion">
-        <a href="nosotros.php">Nosotros</a>
-        <a href="anuncios.php">Anuncios</a>
-        <a href="blog.php">Blog</a>
-        <a href="contacto.php">Contacto</a>
-      </nav><!-- .navegacion -->
-    </div><!-- .contenedor .contenedor__footer-->
-    <p class="copyright">Todos los derechos Reservados 2022 &copy;</p>
-  </footer><!-- .footer .seccion-->
-
+  <?php include 'includes/templates/footer.php'; ?>
 
   <script src="build/js/bundle.min.js"></script>
 </body>
